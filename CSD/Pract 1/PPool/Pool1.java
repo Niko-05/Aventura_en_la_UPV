36f497d5f095fae59d6e8d60b63e82e8e6439187
Niko-05 <nicolasmz0511@gmail.com>
2022-02-17 16:26:47 +0100

    Ejercicios Pool1 y Pool2 completos

--- CSD/Pract 1/PPool/Pool1.java
+++ CSD/Pract 1/PPool/Pool1.java
@@ -1,31 +1,37 @@
 // CSD feb 2015 Juansa Sendra
 
 public class Pool1 extends Pool {   //no kids alone
+    int kidsSwimming = 0;
+    int instructorsSwimming = 0;
     public void init(int ki, int cap){}
     public  synchronized void kidSwims() throws InterruptedException {
-        /*while(true){
+        while(instructorsSwimming == 0){
             log.waitingToSwim();
             wait();
-        }*/
-        
+        }
+        kidsSwimming++;
+        notifyAll();
         log.swimming();
     }
     
     public synchronized void kidRests(){
+        kidsSwimming--;
+        notifyAll();
         log.resting();
-        //this.notifyAll();
     }
     
     public synchronized void instructorSwims(){
+        instructorsSwimming++;
+        notifyAll();
         log.swimming();
-        //this.notifyAll();
     }
     public synchronized void instructorRests() throws InterruptedException {
-        /*while(true){
+        while(kidsSwimming != 0 && instructorsSwimming == 1){
             log.waitingToRest();
             wait();
-        }*/
-        
+        }
+        instructorsSwimming--;
+        notifyAll();
         log.resting();
     }
 }
--- CSD/Pract 1/PPool/Pool2.java
+++ CSD/Pract 1/PPool/Pool2.java
@@ -1,9 +1,38 @@
 // CSD feb 2015 Juansa Sendra
 
 public class Pool2 extends Pool{ //max kids/instructor
-    public void init(int ki, int cap)           {}
-    public void kidSwims()      {log.swimming();}
-    public void kidRests()      {log.resting(); }
-    public void instructorSwims()   {log.swimming();}
-    public void instructorRests()   {log.resting(); }
+    int kidsSwimming = 0;
+    int instructorsSwimming = 0;
+    int KI = 0;
+    public void init(int ki, int cap){KI = ki;}
+    public  synchronized void kidSwims() throws InterruptedException {
+        while(kidsSwimming == KI  * instructorsSwimming){
+            log.waitingToSwim();
+            wait();
+        }
+        kidsSwimming++;
+        notifyAll();
+        log.swimming();
+    }
+    
+    public synchronized void kidRests(){
+        kidsSwimming--;
+        notifyAll();
+        log.resting();
+    }
+    
+    public synchronized void instructorSwims(){
+        instructorsSwimming++;
+        notifyAll();
+        log.swimming();
+    }
+    public synchronized void instructorRests() throws InterruptedException {
+        while(kidsSwimming > KI * instructorsSwimming){
+            log.waitingToRest();
+            wait();
+        }
+        instructorsSwimming--;
+        notifyAll();
+        log.resting();
+    }
 }
