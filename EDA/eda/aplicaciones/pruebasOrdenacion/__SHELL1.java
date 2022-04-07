f0147c59fe52da369b3058fe0e8eee3e06621ed6
Niko-05 <nicolasmz0511@gmail.com>
2022-04-07 13:51:05 +0200

    pract 4 EDA en proceso

--- EDA/eda/aplicaciones/pruebasOrdenacion/__SHELL1.java
+++
@@ -1,8 +0,0 @@
-package aplicaciones.pruebasOrdenacion;
-public class __SHELL1 extends bluej.runtime.Shell {
-public static void run() throws Throwable {
-
-java.lang.String[] __bluej_param0 = { };
-aplicaciones.pruebasOrdenacion.TestOrdenacion.main(__bluej_param0);
-
-}}
--- EDA/eda/aplicaciones/pruebasOrdenacion/__SHELL6.java
+++
@@ -1,7 +0,0 @@
-package aplicaciones.pruebasOrdenacion;
-public class __SHELL6 extends bluej.runtime.Shell {
-public static void run() throws Throwable {
-
-aplicaciones.pruebasOrdenacion.TestOrdenacion.temporizar();
-
-}}
